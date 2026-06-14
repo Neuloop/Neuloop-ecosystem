f7a23362f3b27d09e9197544e3c080c84f0d3689
Neuloop <jirayakewo@gmail.com>
2026-06-14 11:25:41 +0700

    Create Navbar.jsx

---
+++ components/Navbar.jsx
@@ -0,0 +1,13 @@
+export default function Navbar() {
+  return (
+    <nav className="navbar">
+      <h2>NEULOOP</h2>
+
+      <div>
+        <a href="#features">Features</a>
+        <a href="#architecture">Architecture</a>
+        <a href="#roadmap">Roadmap</a>
+      </div>
+    </nav>
+  );
+}
